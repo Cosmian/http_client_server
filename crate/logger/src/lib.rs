f09ee639567979bc2018fd9ac7e3bdb5a4fd787d
Manuthor <manu.coste@gmail.com>
2025-09-08 21:46:57 +0200

    build: release 0.5.2

--- crate/logger/src/lib.rs
+++ crate/logger/src/lib.rs
@@ -17,7 +17,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! cosmian_logger = { version = "0.5.1", features = ["full"] }
+//! cosmian_logger = { version = "0.5.2", features = ["full"] }
 //! ```
 //!
 //! If you get an error like "no `TelemetryConfig` in the root", it means you
